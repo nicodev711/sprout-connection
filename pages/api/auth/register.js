37a61ed1a204821757134450cbc4178039542fea
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-05-24 18:54:55 +0100

    fix auth

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -50,7 +50,7 @@ export default async function handler(req, res) {
         console.time('Set cookie');
         res.setHeader('Set-Cookie', cookie.serialize('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
+            secure: true, // Secure should be true in production
             maxAge: 60 * 60 * 24 * 7, // 1 week
             sameSite: 'strict',
             path: '/'
