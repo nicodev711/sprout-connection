21298aa1b0951b3173ba5fd99083583700b34f25
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-05-31 11:19:19 +0100

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-        <Head>
-            <link rel="icon" href="/logo-noBackground - Copy.png" />
-        </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+  return <Html lang="en">
+      <Head>
+
+          <link rel="icon" href="/logo-noBackground - Copy.png" />
+      </Head>
+    <body>
+      <Main />
+      <NextScript />
+      <GoogleAnalytics gaId="G-QCFYDDKPC2" />
+    </body>
+  </Html>;
 }
