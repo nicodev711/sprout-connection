f31875d8fe9f5e8f7ba218ca1a83b17bad243482
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-05-31 11:41:28 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,13 @@ import {UserProvider} from "@/contexts/UserContext";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from "@/components/Footer";
 import Head from 'next/head'
+import {GoogleAnalytics} from "nextjs-google-analytics";
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
         <Head>
+            <GoogleAnalytics trackPageViews />
             <title>Sprout Connection</title>
         </Head>
         <UserProvider>
