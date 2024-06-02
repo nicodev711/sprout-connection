bb24076377917d41bcc32b49cc5fbcf1e980f343
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-06-03 00:15:52 +0100

    add T&C's, Cookies, Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,18 +4,19 @@ import Script from "next/script";
 export default function Document() {
     return <Html lang="en">
         <Head>
-            <Script src={"https://www.googletagmanager.com/gtag/js?id=G-QCFYDDKPC2"}/>
             <Script id={"analytics"}>
-                {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-QCFYDDKPC2');`}
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M725PM8F');`}
             </Script>
-            <Script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="8baa8391-6083-493a-9e0a-66fb89829879" data-blockingmode="auto" type="text/javascript"/>
-
         </Head>
         <body>
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M725PM8F"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
         <Main/>
         <NextScript/>
         </body>
