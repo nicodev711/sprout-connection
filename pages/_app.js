7db1a37685285136ede998ad3ebe0f4e1cd99343
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-06-03 11:17:21 +0100

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,14 @@ import {UserProvider} from "@/contexts/UserContext";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from "@/components/Footer";
 import Head from 'next/head'
+import {GoogleTagManager} from "@next/third-parties/google";
 
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                <GoogleTagManager gtmId={"GTM-M725PM8F"}/>
                 <link rel="icon" href="/logo-noBackground - Copy.png"/>
                 <title>Sprout Connection</title>
             </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,8 @@
 import {Html, Head, Main, NextScript} from "next/document";
-import { GoogleTagManager } from '@next/third-parties/google'
 export default function Document() {
     return <Html lang="en">
         <Head/>
-        <GoogleTagManager gtmId={"GTM-M725PM8F"}/>
+
         <body>
         <Main/>
         <NextScript/>
