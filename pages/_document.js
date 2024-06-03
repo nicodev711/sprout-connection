861f48fe1c77a48ba83afe7b5a477d7153954854
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-06-03 11:40:16 +0100

    add google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,18 @@
 import {Html, Head, Main, NextScript} from "next/document";
 import {GoogleTagManager} from "@next/third-parties/google";
+import Script from "next/script";
 export default function Document() {
     return <Html lang="en">
         <Head>
             <GoogleTagManager gtmId={"GTM-M725PM8F"}/>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QCFYDDKPC2"/>
+            <Script id={"googleAnalytics"}>
+                {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QCFYDDKPC2');`}
+            </Script>
         </Head>
         <body>
         <Main/>
