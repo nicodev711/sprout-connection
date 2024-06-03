0351774e3cdd63d35303c203e2c6b93ef48f0e08
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-06-03 11:10:29 +0100

    add google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,10 @@ import { GoogleTagManager } from '@next/third-parties/google'
 export default function Document() {
     return <Html lang="en">
         <Head/>
+        <GoogleTagManager gtmId={"GTM-M725PM8F"}/>
         <body>
         <Main/>
         <NextScript/>
         </body>
-        <GoogleTagManager gtmId="GTM-M725PM8F"/>
     </Html>;
 }
