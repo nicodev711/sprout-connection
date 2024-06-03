67b829fa7a242e171c1841466ffb82d6ab23a2c2
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-06-03 10:59:58 +0100

    add google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,6 @@ export default function Document() {
         <body>
         <Main/>
         <NextScript/>
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M725PM8F"
-                    height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe>
-        </noscript>
         </body>
         <GoogleTagManager gtmId="GTM-M725PM8F"/>
     </Html>;
